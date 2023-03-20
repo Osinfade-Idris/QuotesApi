ac852461d0bb4b1a8a458b45f92ce3dd62cb7f81
Idris A. Osinfade <osinfadeidris@gmail.com>
2023-03-20 12:51:53 +0100

    Add project files.

---
+++ QuotesApi/Controllers/QuotesController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using QuotesApi.Data;
+using QuotesApi.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace QuotesApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class QuotesController : ControllerBase
+    {
+        private QuotesDbContext _quotesDbContext;
+        public QuotesController(QuotesDbContext quotesDbContext)
+        {
+            _quotesDbContext = quotesDbContext;
+        }
+
+        // GET: api/<QuotesController>
+        [HttpGet]
+        public IEnumerable<Quote> Get()
+        {
+            return _quotesDbContext.Quotes;
+        }
+
+        // GET api/<QuotesController>/5
+        [HttpGet("{id}")]
+        public Quote Get(int id)
+        {
+            var quote = _quotesDbContext.Quotes.Find(id);
+            return quote;
+        }
+
+        // POST api/<QuotesController>
+        [HttpPost]
+        public void Post([FromBody] Quote quote)
+        {
+            _quotesDbContext.Quotes.Add(quote);
+            _quotesDbContext.SaveChanges();
+        }
+
+        // PUT api/<QuotesController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] Quote quote)
+        {
+           var entity = _quotesDbContext.Quotes.Find(id);
+            entity.Title = quote.Title;
+            entity.Description = quote.Description;
+            entity.Author = quote.Author;
+            _quotesDbContext.SaveChanges();
+        }
+
+        // DELETE api/<QuotesController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            var quote = _quotesDbContext.Quotes.Find(id);
+            _quotesDbContext.Quotes.Remove(quote);
+ 
+        }
+    }
+}
